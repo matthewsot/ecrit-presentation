70bb1dc9bc519996ca77674709fbcf449510b32d
matthewsotoudeh <matthewsot@outlook.com>
2016-01-24 01:15:59 -0800

    basic version of ecrit-presentation

--- ecrit-presentation.js
+++ ecrit-presentation.js
@@ -1,13 +1,37 @@
 ecrit.presentation = {};
-ecrit.presentation.Watcher = function (document) {
+ecrit.presentation.Watcher = function (document, presentationDiv) {
     document.on("nodeInserted", this.nodeInsertionHandler);
     document.on("nodeRemoved", this.nodeRemovalHandler);
 };
 
 ecrit.presentation.Watcher.prototype.nodeInsertionHander = function (data) {
     data.node.on("nodeInserted", this.nodeInsertionHandler);
-    if (data.node.type === "TestSpan") {
-        data.node.on("textModified", this.textModifiedHandler);
+    var parentEl = document.querySelector("[data-ecrit-id=\"" + data.node.parent.id + "\"]");
+    switch (data.node.type) {
+        case "Paragraph":
+            var newEl = document.createElement("p");
+            newEl.setAttribute("data-ecrit-id", data.node.id);
+            
+            if (data.index === parentEl.childNodes.length) {
+                parentEl.appendChild(newEl);
+            }
+            else {
+                parentEl.insertBefore(newEl, parentEl.childNodes[data.index]);
+            }
+            break;
+        case "TextSpan":
+            data.node.on("textModified", this.textModifiedHandler);
+            var newEl = document.createElement("span");
+            newEl.setAttribute("data-ecrit-id", data.node.id);
+            newEl.textContent = data.node.text;
+
+            if (data.index === parentEl.childNodes.length) {
+                parentEl.appendChild(newEl);
+            }
+            else {
+                parentEl.insertBefore(newEl, parentEl.childNodes[data.index]);
+            }
+            break;
     }
 };
 
